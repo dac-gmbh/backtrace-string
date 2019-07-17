260a8e8a1113ee900e40db20af56325035f68445
Philipp Korber <p.korber@dac.eu>
2019-07-17 18:01:51 +0200

    Added tests.

--- src/lib.rs
+++ src/lib.rs
@@ -172,3 +172,140 @@ fn clean_path(p: &Path) -> Cow<Path> {
         p.into()
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use lazy_static::lazy_static;
+
+    use std::{
+        collections::HashMap,
+        panic::{self, PanicInfo, UnwindSafe},
+        sync::{Mutex, Arc, atomic::{AtomicUsize, Ordering}},
+        cell::Cell,
+    };
+
+    type PanicHookFn = dyn Fn(&PanicInfo) + Sync + Send + 'static;
+
+    fn with_panic_hook(hook: Box<PanicHookFn>, func: impl FnOnce() + UnwindSafe) {
+        let reset_id = set_panic_hook(hook);
+        let _ = panic::catch_unwind(func);
+        unset_panic_hook(reset_id);
+
+        ///-----------
+
+        fn set_panic_hook(hook: Box<PanicHookFn>) -> usize {
+            let hook_id = set_hook_id();
+            HOOKS.lock().unwrap().insert(hook_id, hook);
+            hook_id
+        }
+        fn unset_panic_hook(id: usize) {
+            HOOKS.lock().unwrap().remove(&id);
+        }
+        fn set_hook_id() -> usize {
+            let id = HOOK_ID_GEN.fetch_add(1, Ordering::SeqCst);
+            HOOK_ID.with(|id_cell|id_cell.set(id));
+            id
+        }
+        thread_local! {
+            static HOOK_ID: Cell<usize> = Cell::new(0);
+        }
+        static HOOK_ID_GEN: AtomicUsize = AtomicUsize::new(0);
+        lazy_static! {
+            static ref HOOKS: Mutex<HashMap<usize, Box<PanicHookFn>>> = {
+                let old_hook = panic::take_hook();
+                panic::set_hook(Box::new(move |panic_info| {
+                    old_hook(panic_info);
+                    let id = HOOK_ID.with(|id| id.get());
+                    if let Some(hook) = HOOKS.lock().unwrap().get(&id) {
+                        hook(panic_info);
+                    }
+                }));
+
+                Mutex::new(HashMap::new())
+            };
+        }
+    }
+
+    fn backtrace_from_panic_hook(inner: impl FnOnce() + UnwindSafe) -> String {
+        let result_cell = Arc::new(Mutex::new(None));
+        let result_cell2 = result_cell.clone();
+        let hook = Box::new(move |panic_info: &PanicInfo| {
+            let out = crate::create_backtrace();
+            *result_cell.lock().unwrap() = Some(out);
+        });
+
+        with_panic_hook(hook, inner);
+
+        let mut cell = result_cell2.lock().unwrap();
+        cell.take().unwrap()
+    }
+
+    #[test]
+    fn backtrace_in_panic_hook() {
+        let bt = backtrace_from_panic_hook(|| panic!("test backtrace from panic hook"));
+        assert!(bt.trim().len() > 0);
+    }
+
+    // Note: This tests might brake/start failing with **non braking changes** in rustc and/or std
+    #[test]
+    fn instable_backtrace_in_panic_hook() {
+        let bt = backtrace_from_panic_hook(|| panic!("test backtrace from panic hook"));
+        let expected_bt = r#"
+            0: std::panic::catch_unwind::{@}
+                at /rustc/{@}/src/libstd/panic.rs:{@}
+            1: backtrace_string::tests::with_panic_hook::{@}
+                at src/lib.rs:{@}
+            2: backtrace_string::tests::backtrace_from_panic_hook::{@}
+                at src/lib.rs:{@}
+            3: backtrace_string::tests::instable_backtrace_in_panic_hook::{@}
+                at src/lib.rs:{@}
+            4: backtrace_string::tests::instable_backtrace_in_panic_hook::{{closure}}::{@}
+                at src/lib.rs:{@}
+            5: core::ops::function::FnOnce::call_once::{@}
+                at /rustc/{@}/src/libcore/ops/function.rs:{@}
+            6: <alloc::boxed::Box<F> as core::ops::function::FnOnce<A>>::call_once::{@}
+                at /rustc/{@}/src/liballoc/boxed.rs:{@}
+            7: __rust_maybe_catch_panic
+                at src/libpanic_unwind/lib.rs:{@}
+            8: std::panicking::try::{@}
+                at /rustc/{@}/src/libstd/panicking.rs:{@}
+               std::panic::catch_unwind::{@}
+                at /rustc/{@}/src/libstd/panic.rs:{@}
+               test::run_test::run_test_inner::{{closure}}::{@}
+                at src/libtest/lib.rs:{@}
+        "#;
+
+        fuzzy_stacktrace_eq(expected_bt, bt);
+    }
+
+    fn fuzzy_stacktrace_eq(expected: &'static str, got: String) {
+        let mut exp_lines = expected.trim().lines()
+            .map(|line| line.trim());
+        let mut got_lines = got.trim().lines()
+            .map(|line| line.trim());
+
+        loop {
+            let (exp, mut got) = match (exp_lines.next(), got_lines.next()) {
+                (Some(exp), Some(got)) => (exp, got),
+                (Some(exp), None) => panic!("expected backtrace has additional lines, starting with {:?}", exp),
+                (None, Some(got)) => panic!("created backtrace has additional lines, starting with {:?}", got),
+                (None, None) => break
+            };
+
+            dbg!(exp);
+            dbg!(got);
+
+            for part in exp.split("{@}") {
+                if !got.starts_with(part) {
+                    panic!("Mismatch {:?} should start with {:?}", got, part);
+                }
+
+                got = &got[part.len()..];
+
+                got = got.trim_start_matches(|c: char| c.is_ascii_alphanumeric());
+            }
+        }
+    }
+
+}
